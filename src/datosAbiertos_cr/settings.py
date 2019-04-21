8d279984135cff71cda0edcba372d8226c14ba8b
Alejandro <alegamboaba.17@gmail.com>
2019-04-21 10:01:49 +0200

    new version of settings.py

--- src/datosAbiertos_cr/settings.py
+++ src/datosAbiertos_cr/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
     }
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
