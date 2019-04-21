157a87625fc77b359e534df06479edcea0a98d83
Alejandro <alegamboaba.17@gmail.com>
2019-04-21 09:23:05 +0200

    Merge with master

--- src/app_licitaciones/urls.py
+++ src/app_licitaciones/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from app_licitaciones.views import get_data_colones, get_data_dolar, load_index, get_all_data, load_information
+from app_licitaciones.views import get_data_colones, get_data_dolar, load_index, get_all_data, load_information, \
+    load_alldata_html
 
 urlpatterns = [
     path('', load_index, name='index'),
+    path('licitaciones/', load_alldata_html, name='licitaciones'),
     path('allData/', get_all_data, name='allData'),
     path('DatosDolar/', get_data_dolar, name='dolars'),
     path('DatosColones/', get_data_colones, name='colones'),
--- src/app_licitaciones/views.py
+++ src/app_licitaciones/views.py
@@ -4,6 +4,7 @@ import json
 import requests
 import pandas as pd
 from pandas.io.json import json_normalize
+import io
 
 # Create your views here.
 api_key = '4ac87f27590e5046eda6f7cedc638ce20124746b'
@@ -11,6 +12,16 @@ url_base = 'http://api.datosabiertos.presidencia.go.cr/api/v2/datastreams/'
 guid = 'LICIT-ADJUD-DE-LAS-INSTI'
 urlApi = url_base + guid + '/data.ajson/?auth_key=' + api_key + '&limit=50&'
 response = requests.get(urlApi)
+pd.set_option('max_colwidth', 100)
+
+
+def load_index(request):
+    """
+    This function render the index.html, this is the main page
+    :param request:
+    :return: Render index.html
+    """
+    return render(request, 'index.html')
 
 
 def get_data_dolar(request):
@@ -41,20 +52,46 @@ def get_data_colones(request):
 
 def get_all_data(request):
     if response.status_code == 200:
+        data = getAll_data()
+        datoshtml = data['htmlT']
+
+        return HttpResponse(datoshtml)
+
+
+def getAll_data():
+    dataF = {}
+    if response.status_code == 200:
         data = json.loads(response.content)
         nycphil = json_normalize(data, ['result'])
         allData = pd.DataFrame(nycphil)
+        allData.columns = ['Año', 'Institución', 'Tipo Trámite', 'Proveedor Adjudicado', 'Fecha de Adjudicación',
+                           'Moneda', 'Monto Adjudicado']
+        allData.drop(allData.index[[0]], inplace=True)
+        allData['Año'] = pd.to_numeric(allData['Año'], errors='coerce')
+        buf = io.StringIO()
+        allData.info(buf=buf)
+        show_info = buf.getvalue()
+        dataF['Info'] = show_info
+        dataF['Dimensiones'] = allData.shape
+        dataF['Columnas'] = allData.columns.values
+        dataF['htmlT'] = allData.to_html()
+        allData
+        return dataF
 
-        return HttpResponse(allData.to_html())
 
-
-def load_index(request):
-    """
-    This function render the index.html, this is the main page
-    :param request:
-    :return: Render index.html
-    """
-    return render(request, 'index.html')
+def load_alldata_html(request):
+    alldataLic = getAll_data()
+    dim = alldataLic['Dimensiones']
+    info = alldataLic['Info']
+    columnas = alldataLic['Columnas']
+    htmlT = alldataLic['htmlT']
+    context = {
+        'columnas': columnas,
+        'info': info,
+        'dimensiones': dim,
+        'tablahtmlT': htmlT,
+    }
+    return render(request, 'observLicitaciones.html', context)
 
 
 def load_information(request):
@@ -63,4 +100,5 @@ def load_information(request):
     :param request:
     :return: Render information.html
     """
+
     return render(request, 'information.html')
