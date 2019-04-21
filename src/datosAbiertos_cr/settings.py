fe4d9d3c44f1b7c8ba7c980e5ae7eecd6ec0ac56
Alejandro <alegamboaba.17@gmail.com>
2019-04-21 10:00:23 +0200

    new version of settings.py

--- src/datosAbiertos_cr/settings.py
+++ src/datosAbiertos_cr/settings.py
@@ -115,12 +115,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
