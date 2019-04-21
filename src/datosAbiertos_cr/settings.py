ec8ef4fdb3c7361bf9b4469fe6e37ffe067f430e
Alejandro <alegamboaba.17@gmail.com>
2019-04-21 09:50:20 +0200

    test settings hosts

--- src/datosAbiertos_cr/settings.py
+++ src/datosAbiertos_cr/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'j!u0k+3x&41&*@dsyh+ts_c0rc1cvqep3(re)%@pc9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['opendatacrweb.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
