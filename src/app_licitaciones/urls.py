135fe3f1e89e2bb9f3a5d49ce2f73b0b88d911f8
Alejandro <alegamboaba.17@gmail.com>
2019-04-21 10:38:46 +0200

    Fixing Settings

--- src/app_licitaciones/urls.py
+++ src/app_licitaciones/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from app_licitaciones.views import get_data_colones, get_data_dolar, load_index, get_all_data, load_information, \
     load_alldata_html
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', load_index, name='index'),
@@ -10,4 +12,4 @@ urlpatterns = [
     path('DatosColones/', get_data_colones, name='colones'),
     path('information/', load_information, name='information'),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
